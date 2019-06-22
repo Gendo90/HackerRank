9146cebb00d8163078eba0e3041598d09ace4bd3
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-06-22 14:35:53 -0700

    feat: Add solution to "Reverse Array" HackerRank problem
    Note: It was relatively simple, but the output format threw me for a
    minute because of how they modify the function output and put it in
    the correct form if the output is an array

---
+++ Arrays/reverseArray.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the reverseArray function below.
+def reverseArray(a):
+    a.reverse()
+    return a
+
+print(reverseArray([1, 4, 3, 2]))
+# Note that the code to format the input/output takes an array as the
+# output of this function and then formats it - I was formatting it in the
+# function and it said I was wrong...
