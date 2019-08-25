07738af1822ff996406e80002743042996149402
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-08-24 18:12:04 -0700

    feat: Add a bunch of basic linked list operation solutions to problems/quizzes from HackerRank
    Note: Problems are part of "MyCodeSchool" apparently

---
+++ Linked Lists/insertNodeAtPosition.py
@@ -0,0 +1,87 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtTail function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtPosition(head, data, position):
+    node = SinglyLinkedListNode(data)
+    if(position==0):
+        node.next = head
+        return node
+
+    count = 0
+    current = head
+    while(count<position-1):
+        current=current.next
+        count+=1
+
+    node.next = current.next
+    current.next = node
+
+    return head
+
+
+
+
+# if __name__ == '__main__':
+#     # n = int(input())
+#     fileHandler = open("insertTailNode_test_case.txt", "r")
+#
+#     # Get list of all lines in file
+#     listOfLines = fileHandler.readlines()
+#
+#     # Close file
+#     fileHandler.close()
+#
+#     llist = SinglyLinkedList()
+#
+#     gb = []
+#     for i, line in enumerate(listOfLines):
+#         if(i==0):
+#             continue
+#         gb.append(int(line))
+#
+#     print(sys.getrecursionlimit())
+#     #note had to change the recursion limit here to get it to work!
+#     sys.setrecursionlimit(5000)
+#
+#     for i in range(len(gb)):
+#         llist_item = gb[i]
+#         llist_head = insertNodeAtTail(llist.head, llist_item)
+#         llist.head = llist_head
+#
+#     node = llist.head
+#     while(node):
+#         print(node.data)
+#         node = node.next
---
+++ Linked Lists/reverse.py
@@ -0,0 +1,92 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtTail function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def reverse(head):
+    if(not head):
+        return
+
+    current = head
+
+    store_stack = [current]
+    while(current):
+        current = current.next
+        store_stack.append(current)
+
+    store_stack.pop()
+    new_current = store_stack.pop()
+    head = new_current
+    while(store_stack):
+        new_current.next = store_stack.pop()
+        new_current = new_current.next
+
+    new_current.next = None
+
+    return head
+
+
+
+
+# if __name__ == '__main__':
+#     # n = int(input())
+#     fileHandler = open("insertTailNode_test_case.txt", "r")
+#
+#     # Get list of all lines in file
+#     listOfLines = fileHandler.readlines()
+#
+#     # Close file
+#     fileHandler.close()
+#
+#     llist = SinglyLinkedList()
+#
+#     gb = []
+#     for i, line in enumerate(listOfLines):
+#         if(i==0):
+#             continue
+#         gb.append(int(line))
+#
+#     print(sys.getrecursionlimit())
+#     #note had to change the recursion limit here to get it to work!
+#     sys.setrecursionlimit(5000)
+#
+#     for i in range(len(gb)):
+#         llist_item = gb[i]
+#         llist_head = insertNodeAtTail(llist.head, llist_item)
+#         llist.head = llist_head
+#
+#     node = llist.head
+#     while(node):
+#         print(node.data)
+#         node = node.next
