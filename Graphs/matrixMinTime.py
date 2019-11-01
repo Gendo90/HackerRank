67eede1d556faee62c42d07b80f66ac140a355d8
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-11-01 14:14:42 -0700

    feat: Add better solution to "Matrix" problem from HackerRank
    Note: Need to keep searching remnants of broken-down graph until
    all machines are disconnected

--- Graphs/matrixMinTime.py
+++ Graphs/matrixMinTime.py
@@ -7,13 +7,28 @@ import re
 import sys
 import time
 
+def bfs(start, graph, machine_map):
+    queue = [(start, 0)]
+    seen = {start:True}
 
+    while(queue):
+        curr = queue.pop(0)
+        if(curr[0] in machine_map and curr[0]!=start):
+            print(start, curr[0])
+            return True
+        for item in graph[curr[0]]:
+            if(item not in seen):
+                queue.append(item)
+                seen[item] = True
+
+    return False
 
 
 def dijkstra_mapping(start, graph, machine_map):
     this_seen = {start:([(start, 0)])}
     path = [start]
     all_possible = {}
+    count = 100
 
     while(((len(path)==1 and graph[start]) or path[-1] not in machine_map)):
         time_limit = 10000000
@@ -35,14 +50,17 @@ def dijkstra_mapping(start, graph, machine_map):
         # print(all_possible.values())
         all_possible = this_seen.copy()
         path.append(next_city)
-        print(graph)
+        # print(graph)
         this_seen[next_city] = this_seen[last_city][:]
         this_seen[next_city].append((next_city, cut_time))
         if(all_possible==this_seen):
-            print(all_possible, this_seen)
-            print("oh no", flush=True)
-            return [1], [1], [0]
-        print(this_seen)
+            count-=1
+            if(count<0):
+            # print(all_possible, this_seen)
+            # print("oh no", flush=True)
+                print("skipped", start)
+                return [1], [1], [0]
+        # print(this_seen)
 
     return path, this_seen[path[-1]], path[-1]
 
@@ -73,7 +91,6 @@ def minTime(roads, machines):
         graph_map[city2].add((city1, time))
 
     for city in machine_map.keys():
-        print(city, flush=True)
         path, times, curr = dijkstra_mapping(city, graph_map, machine_map)
         # print(times)
         # print(graph_map)
@@ -89,8 +106,33 @@ def minTime(roads, machines):
         second_removal = second_removal_list.pop()
         graph_map[removal[0]].remove(second_removal)
         graph_map[second_removal[0]].remove(removal)
+        # print(city, removal[1], flush=True)
 
         time_total+=removal[1]
+
+    #gives remaining items to be disconnected
+    print(graph_map)
+    print(machine_map)
+    for item in machine_map.keys():
+        if(bfs(item, graph_map, machine_map)):
+            path, times, curr = dijkstra_mapping(item, graph_map, machine_map)
+            # print(times)
+            # print(graph_map)
+            starter = times.pop(0)
+            #need to sever the connection here at the minimum edge
+            times.sort(key=sorter_func)
+            if(not times):
+                continue
+            removal = times[0]
+            # print(removal)
+            times.append(starter)
+            second_removal_list = [a for a in graph_map[removal[0]] if (a[0] in list(zip(*times))[0] and a[1]==removal[1])]
+            second_removal = second_removal_list.pop()
+            graph_map[removal[0]].remove(second_removal)
+            graph_map[second_removal[0]].remove(removal)
+            # print(city, removal[1], flush=True)
+
+            time_total+=removal[1]
 
     return time_total
 
@@ -101,7 +143,7 @@ def minTime(roads, machines):
 # r = [(0, 1, 4), (1, 2, 3), (1, 3, 7), (0, 4, 2)]
 # k = [2, 3, 4]
 # print(minTime(r, k))
-
+#
 if __name__ == '__main__':
 
     fileHandler = open("minTime_TestCase2.txt", "r")
