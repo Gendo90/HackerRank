18edeab0ef9740ff74c3034fbe4d21bc31a63be7
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-12-12 13:01:32 -0800

    feat: Add support for non-distinct (duplicate) values in the HeapMap data structure

--- Heaps/qheap1_upgraded.py
+++ Heaps/qheap1_upgraded.py
@@ -11,12 +11,20 @@ class HeapMap():
     def push(self, elem):
         self.heap.append(elem)
         i=len(self.heap)
-        self.indices[elem] = i-1
+        #use lists instead of single values for the map from values to indices
+        if(elem not in self.indices):
+            self.indices[elem] = [i-1]
+        else:
+            self.indices[elem].append(i-1)
+            print(self.indices[elem])
         #bubble-up through heap
         while(elem<self.heap[i//2-1] and i>1):
             #update map of values with new indices
             parent_elem = self.heap[i//2-1]
-            self.indices[elem], self.indices[parent_elem] = i//2-1, i-1
+            self.indices[elem][-1] = i//2-1
+            #need to replace the correct parent element index in the list
+            #for multiple indices with the same value
+            self.indices[parent_elem][self.indices[parent_elem].index(i//2-1)] = i-1
             #update element positions in heap array
             self.heap[i-1], self.heap[i//2-1] = self.heap[i//2-1], self.heap[i-1]
             i=i//2
@@ -25,13 +33,15 @@ class HeapMap():
         result = self.heap.pop(0)
         if(not self.heap):
             return result
+        #get the length of the heap
+        heap_size = len(self.heap)
+
         #replace first element with the last element in balanced heap
         last_elem = self.heap.pop()
         self.heap.insert(0, last_elem)
-        self.indices[last_elem] = 0
+        self.indices[last_elem][self.indices[last_elem].index(heap_size)] = 0
         #bubble down through heap to get new minimum at position 0
         i=1
-        heap_size = len(self.heap)
         while(i<heap_size):
             if(2*i<=heap_size):
                 first_child_ind = 2*i-1
@@ -48,10 +58,13 @@ class HeapMap():
                 min_child_ind = second_child_ind
             #switch min child with the parent unless parent is larger
             parent = self.heap[i-1]
-            if(self.heap[min_child_ind]>=parent):
+            child = self.heap[min_child_ind]
+            if(child>=parent):
                 break
             else:
-                self.indices[self.heap[min_child_ind]], self.indices[parent] = i-1, min_child_ind
+                #update values to indices map as an array
+                self.indices[child][self.indices[child].index(min_child_ind)] = i-1
+                self.indices[parent][self.indices[parent].index(i-1)] = min_child_ind
                 self.heap[min_child_ind], self.heap[i-1] = self.heap[i-1], self.heap[min_child_ind]
             i=min_child_ind+1
 
@@ -59,15 +72,19 @@ class HeapMap():
     def remove(self, elem):
         if(elem not in self.indices):
             return False
-        removal_ind = self.indices[elem]
+        removal_ind = self.indices[elem][-1]
         self.heap[removal_ind] = -float('inf')
-        self.indices.pop(elem)
+        #remove index from map, and if there are no indices left, remove value
+        #key from map
+        self.indices[elem].pop()
+        if(not self.indices[elem]):
+            self.indices.pop(elem)
         #bubble up the -inf value and then remove it!
         i=removal_ind+1
         while(i!=1):
             #update map of values with new parent index as it moves down in the heap
             parent_elem = self.heap[i//2-1]
-            self.indices[parent_elem] = i-1
+            self.indices[parent_elem][self.indices[parent_elem].index(i//2-1)] = i-1
             #update element positions in heap array
             self.heap[i-1], self.heap[i//2-1] = self.heap[i//2-1], self.heap[i-1]
             i=i//2
@@ -105,4 +122,5 @@ if __name__ == "__main__":
             if(results[0]==1):
                 heap.push(results[1])
             else:
+                # print(results[1], heap.heap, heap.indices)
                 heap.remove(results[1])
