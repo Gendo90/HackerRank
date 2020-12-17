ff001b80ac15b0376ed9ebbd071e700ad2045006
Patrick Gendotti <patrick.gendotti@gmail.com>
2020-12-17 11:55:06 -0800

    feat: Get basic calculator working for equals and add operation - now need to add the other operations!

--- 10 Days of JS/binary_calc/js/binaryCalculator.js
+++ 10 Days of JS/binary_calc/js/binaryCalculator.js
@@ -4,6 +4,34 @@ model = {
     operation: ''
 }
 
+operations = {
+    '+': (a, b) => a+b,
+    '-': (a, b) => a-b,
+    '/': (a, b) => Math.floor(a/b),
+    '*': (a, b) => a*b,
+}
+
+function toBinary(x) {
+    let output = ''
+    //first find max power of 2 that is larger than the number
+    let max_pow = 0
+    //always overshoots by one
+    while(Math.pow(2, max_pow) <= x) {
+        max_pow += 1
+    }
+    max_pow -= 1
+    for (let i = max_pow; i >= 0; i--) {
+        if(x - Math.pow(2, i) >= 0) {
+            output += '1'
+            x -= Math.pow(2, i)
+        }
+        else {
+            output += '0'
+        }
+    }
+    return output
+}
+
 //get result display element
 resEl = document.getElementById("res")
 
@@ -28,9 +56,24 @@ clearButton.addEventListener("click", (e) => {
 addButton = document.getElementById("btnSum")
 addButton.addEventListener("click", (e) => {
     if(model.currVal) {
-        operation = '+'
+        model.operation = '+'
         resEl.textContent += '+'
         model.lastVal = model.currVal
         model.currVal = ''
+    }
+})
+
+
+//add equals button functionality
+equalsButton = document.getElementById("btnEql")
+equalsButton.addEventListener("click", (e) => {
+    console.log(model)
+    if(model.operation && model.lastVal && model.currVal) {
+        let result = operations[model.operation](parseInt(model.lastVal, 2), parseInt(model.currVal, 2))
+        console.log(result)
+        result = toBinary(result)
+        console.log(result)
+        model.lastVal = result
+        resEl.textContent = result
     }
 })
